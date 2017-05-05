1c8017c716f10fcb4016bf85c5ec1bf44cfe1d38
Luis del Cueto <luisdelcueto@live.com>
2017-05-05 15:23:50 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    x = 0
+    while x < self.characters.size
+      roles << "#{self.characters[x].name} - #{self.shows[x].name}"
+      x += 1
+    end
+    roles
+  end
   
 end
